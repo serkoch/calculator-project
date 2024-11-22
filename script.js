aecf9ce408a6b0ffa5f337c55fefd8f438bbc4c7
nevermind <nevermind359@gmail.com>
2024-11-22 16:04:49 -0500

    fix: Fix operation chaining
    Still struggling with handling division by 0

--- script.js
+++ script.js
@@ -13,12 +13,17 @@ function multiply(x, y) {
 function divide(x, y) {
   if (y === 0) {
     return 'ERROR!';
+  } else {
+    return (x /= y);
   }
-  return (x /= y);
 }
 
 function calculate(op, x, y) {
-  return op(Number(x), Number(y));
+  if (op(x, y) === 'ERROR!') {
+    return 'ERROR!';
+  } else {
+    return Math.floor(op(Number(x), Number(y)) * 1000) / 1000;
+  }
 }
 
 const screen = document.querySelector('.screen-operand');
@@ -38,21 +43,7 @@ const equals = document.querySelector('#equals');
 
 let storedOperand = null;
 let currentOperand = null;
-let storedOperator = '';
-
-nums.forEach((num) => {
-  num.addEventListener('click', function () {
-    if (screen.textContent === 'ERROR!') {
-      storedOperand = null;
-      currentOperand = null;
-      screen.textContent = '';
-      opScreen.textContent = '';
-    }
-    if (screen.textContent.length < 8) {
-      return (screen.textContent += num.id);
-    }
-  });
-});
+let storedOperator = null;
 
 function getStoredOperand(num) {
   return (storedOperand = Number(num));
@@ -65,52 +56,80 @@ function getCurrentOperand(num) {
 function clearAll() {
   storedOperand = null;
   currentOperand = null;
-  screen.textContent = '';
-  opScreen.textContent = '';
+  storedOperator = null;
+  screen.textContent = null;
+  opScreen.textContent = null;
 }
 
 function calculation(operator) {
+  currentOperand = getCurrentOperand(screen.textContent);
   switch (operator) {
     case 'add':
       storedOperand = calculate(add, storedOperand, currentOperand);
-      screen.textContent = storedOperand;
+      currentOperand = null;
+      storedOperator = null;
       break;
     case 'subtract':
       storedOperand = calculate(subtract, storedOperand, currentOperand);
-      screen.textContent = storedOperand;
+      currentOperand = null;
+      storedOperator = null;
       break;
     case 'multiply':
       storedOperand = calculate(multiply, storedOperand, currentOperand);
-      screen.textContent = storedOperand;
+      currentOperand = null;
+      storedOperator = null;
       break;
     case 'divide':
       storedOperand = calculate(divide, storedOperand, currentOperand);
-      screen.textContent = storedOperand;
+      currentOperand = null;
+      storedOperator = null;
       break;
   }
 }
 
-equals.addEventListener('click', function () {
-  if (storedOperand !== null) {
-    currentOperand = getCurrentOperand(screen.textContent);
-    calculation(storedOperator);
-    opScreen.textContent = equals.textContent;
-  }
+nums.forEach((num) => {
+  num.addEventListener('click', function () {
+    if (screen.textContent === 'ERROR!') {
+      clearAll();
+      return (screen.textContent += num.id);
+    } else if (screen.textContent.length < 8) {
+      return (screen.textContent += num.id);
+    }
+  });
 });
 
 operators.forEach((operator) => {
   operator.addEventListener('click', function () {
     opScreen.textContent = '';
-    storedOperator = operator.id;
-    if (storedOperand === null) {
+    if (screen.textContent === 'ERROR!') {
+      clearAll();
+    } else if (storedOperand === null && screen.textContent === '') {
+      storedOperator = operator.id;
+      opScreen.textContent = operator.textContent;
+    } else if (storedOperand === null) {
       storedOperand = getStoredOperand(screen.textContent);
-      screen.textContent = null;
-    } else {
-      screen.textContent = null;
-      currentOperand = getCurrentOperand(screen.textContent);
+      storedOperator = operator.id;
+      screen.textContent = '';
+      opScreen.textContent = operator.textContent;
+    } else if (storedOperand !== null && screen.textContent === '') {
+      opScreen.textContent = operator.textContent;
+      storedOperator = operator.id;
+      screen.textContent = '';
+    } else if (storedOperand !== null && screen.textContent !== '') {
+      calculation(storedOperator);
+      screen.textContent = '';
+      opScreen.textContent = operator.textContent;
+      storedOperator = operator.id;
     }
-    return (opScreen.textContent += operator.textContent);
   });
+});
+
+equals.addEventListener('click', function () {
+  if (storedOperand !== null && storedOperator !== null) {
+    calculation(storedOperator);
+    screen.textContent = storedOperand;
+    opScreen.textContent = equals.textContent;
+  }
 });
 
 decimal.addEventListener('click', function () {
@@ -120,7 +139,9 @@ decimal.addEventListener('click', function () {
 });
 
 sign.addEventListener('click', function () {
-  if (screen.textContent.indexOf('-') === -1) {
+  if (screen.textContent === 'ERROR!') {
+    clearAll();
+  } else if (screen.textContent.indexOf('-') === -1) {
     return (screen.textContent = '-' + screen.textContent);
   } else {
     return (screen.textContent = screen.textContent.substring(1));
